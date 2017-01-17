6ffabc54ed7924932913cc1d8f2f2ea2d4a4beee
yifeiyangecnu <guang96@163.com>
2017-01-17 15:28:21 +0800

    改写

--- setMenu/src/main/java/com/yangyifei/wechat/util/MenuUtil.java
+++ setMenu/src/main/java/com/yangyifei/wechat/util/MenuUtil.java
@@ -9,19 +9,19 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.yangyifei.wechat.Exception.MyException;
 
 public class MenuUtil {
-	 private static  String ACCESS_TOKEN="123";
+	 private static  String ACCESS_TOKEN="";
 	 private static final String SET_MENU_URL="https://api.weixin.qq.com/cgi-bin/menu/create?";
-	 public static boolean setMenu(String jsonPram) throws ClientProtocolException, IOException, MyException
+	 public static boolean setMenu(String jsonPram, String appId, String appSecret) throws ClientProtocolException, IOException, MyException
 	 {
-		 ACCESS_TOKEN=Access_TokenUtil.getToken();
-		 String url=SET_MENU_URL+"access_token="+ACCESS_TOKEN+"&body="+jsonPram;
-		System.out.println(url);
+		 ACCESS_TOKEN=Access_TokenUtil.getToken(appId, appSecret);
+		 String url=SET_MENU_URL+"access_token="+ACCESS_TOKEN;
 		 ObjectMapper objectMapper=new ObjectMapper();
-		 JsonNode response = objectMapper.readTree(ConnectUtil.postRequest(url));
+		 JsonNode response = objectMapper.readTree(ConnectUtil.postRequestWithBody(url,jsonPram));
 		String errmsg=response.get("errmsg").asText();
 		 int errcode=response.get("errcode").asInt();
 		 if(errcode==0)
 		 {
+			 System.out.println("...菜单已成功设置...\n...会有延迟");
 			 return true;
 		 }
 		 else
