d62366786e20d1d9eb3dbf84e94981aa88d24174
yifeiyangecnu <guang96@163.com>
2017-01-17 15:27:47 +0800

    改写

--- setMenu/src/main/java/com/yangyifei/wechat/util/ConnectUtil.java
+++ setMenu/src/main/java/com/yangyifei/wechat/util/ConnectUtil.java
@@ -2,12 +2,12 @@ package com.yangyifei.wechat.util;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
 
+import org.apache.http.HttpEntity;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 
@@ -16,24 +16,24 @@ public class ConnectUtil {
 	{
         HttpClientBuilder httpClientBuilder = HttpClientBuilder.create();  
         CloseableHttpClient httpClient = httpClientBuilder.build(); 
-		HttpPost post=new HttpPost(urlEncode(url));
+		HttpPost post=new HttpPost(url);
 		return httpClient.execute(post).getEntity().getContent();
+	}
+	public static InputStream postRequestWithBody(String url,String body) throws UnsupportedOperationException, ClientProtocolException, IOException
+	{
+		  HttpClientBuilder httpClientBuilder = HttpClientBuilder.create();  
+	        CloseableHttpClient httpClient = httpClientBuilder.build(); 
+			HttpPost post=new HttpPost(url);
+			HttpEntity entity=new StringEntity(body,"UTF-8");
+			post.setEntity(entity);
+			return httpClient.execute(post).getEntity().getContent();
 	}
 	public static InputStream getRequest(String url) throws UnsupportedOperationException, ClientProtocolException, IOException
 	{
 		HttpClientBuilder httpClientBuilder=HttpClientBuilder.create();
 		 CloseableHttpClient httpClient = httpClientBuilder.build(); 
-		 HttpGet get=new HttpGet(urlEncode(url));
+		 HttpGet get=new HttpGet(url);
 		 return httpClient.execute(get).getEntity().getContent();
 	}
 	
-	private static String urlEncode(String url) throws UnsupportedEncodingException
-	{
-		int pos=url.indexOf('?');
-		if(pos>0)
-		{
-			return url.substring(0,pos)+URLEncoder.encode(url.substring(pos),"UTF-8");
-		}
-		return url;
-	}
 }
